e40a023e963c50ec9d8bb5b0031491754e90c976
MVDG30 <83533843+MVDG30@users.noreply.github.com>
2022-01-02 10:15:18 +0100

    Edited weather components

--- src/Temperature.js
+++ src/Temperature.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function Temperature() {
   return "4";
 }
--- src/Time.js
+++ src/Time.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function Time() {
   return "19:30";
 }
